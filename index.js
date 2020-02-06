8ab229cfe69de239b006d0bfc598038945850f5f
theo <theophile.vast@amadeus.com>
2020-02-06 12:57:02 +0100

    update apckage

--- index.js
+++ index.js
@@ -141,110 +141,7 @@ const flightPrice = async function flifghtPrice(uriApi,NaseUrl,inputFlightOffer,
 
 const createOrder =async function createOrder(NaseUrl,uriApi,flightOffer , mail,token) {
 
-  let request = 
- {
-  "data": {
-    "type": "flight-order",
-    "flightOffers": [flightOffer],
-    "travelers": [
-      {
-        "id": "1",
-        "dateOfBirth": "1982-01-16",
-        "name": {
-          "firstName": "JORGE",
-          "lastName": "GONZALES"
-        },
-        "gender": "MALE",
-        "contact": {
-          "emailAddress": mail,
-          "phones": [
-            {
-              "deviceType": "MOBILE",
-              "countryCallingCode": "34",
-              "number": "480080076"
-            }
-          ]
-        },
-        "documents": [
-          {
-            "documentType": "PASSPORT",
-            "birthPlace": "Madrid",
-            "issuanceLocation": "Madrid",
-            "issuanceDate": "2015-04-14",
-            "number": "00000000",
-            "expiryDate": "2025-04-14",
-            "issuanceCountry": "ES",
-            "validityCountry": "ES",
-            "nationality": "ES",
-            "holder": true
-          }
-        ]
-      },
-      {
-        "id": "2",
-        "dateOfBirth": "2012-10-11",
-        "gender": "FEMALE",
-        "contact": {
-          "emailAddress": "jorge.gonzales833@telefonica.es",
-          "phones": [
-            {
-              "deviceType": "MOBILE",
-              "countryCallingCode": "34",
-              "number": "480080076"
-            }
-          ]
-        },
-        "name": {
-          "firstName": "ADRIANA",
-          "lastName": "GONZALES"
-        }
-      }
-    ],
-    "remarks": {
-      "general": [
-        {
-          "subType": "GENERAL_MISCELLANEOUS",
-          "text": "ONLINE BOOKING FROM INCREIBLE VIAJES"
-        }
-      ]
-    },
-    "ticketingAgreement": {
-      "option": "DELAY_TO_CANCEL",
-      "delay": "6D"
-    },
-    "contacts": [
-      {
-        "addresseeName": {
-          "firstName": "PABLO",
-          "lastName": "RODRIGUEZ"
-        },
-        "companyName": "INCREIBLE VIAJES",
-        "purpose": "STANDARD",
-        "phones": [
-          {
-            "deviceType": "LANDLINE",
-            "countryCallingCode": "34",
-            "number": "480080071"
-          },
-          {
-            "deviceType": "MOBILE",
-            "countryCallingCode": "33",
-            "number": "480080072"
-          }
-        ],
-        "emailAddress": "support@increibleviajes.es",
-        "address": {
-          "lines": [
-            "Calle Prado, 16"
-          ],
-          "postalCode": "28014",
-          "cityName": "Madrid",
-          "countryCode": "ES"
-        }
-      }
-    ]
-  }
-}
+  
 // console.log(NaseUrl+uriApi)
   // Default options are marked with *
   const response = await fetch(NaseUrl+uriApi, {
@@ -258,7 +155,7 @@ const createOrder =async function createOrder(NaseUrl,uriApi,flightOffer , mail,
     },
     redirect: 'follow', // manual, *follow, error
     referrerPolicy: 'no-referrer', // no-referrer, *client
-    body: JSON.stringify(request) // body data type must match "Content-Type" header
+    body: JSON.stringify(flightOffer) // body data type must match "Content-Type" header
   });
   // console.log(request)
   return await response.json(); // parses JSON response into native JavaScript objects
